2e73be388e64141d346be6d942d475e43b447b02
Kevin Greenwood <kevinrecalde324@outlook.com>
2025-12-17 12:09:33 -0500

    Create main.dart

---
+++ example/main.dart
@@ -0,0 +1,18 @@
+import 'package:gplay_store_scrapper/gplay_store_scrapper.dart';
+
+void main() async
+{
+  final scrapper = Scrapper();
+
+  final result = await scrapper.fetchAppDetails('com.spotify.music');
+
+  print('App Name: ${result['appName']}');
+  print('Author: ${result['author']}');
+  print('Genre: ${result['genre']}');
+  print('Icon Path: ${result['iconPath']}');
+
+  if (result.containsKey('error'))
+  {
+    print('Error: ${result['error']}');
+  }
+}
